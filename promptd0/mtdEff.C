6d65573797ce2ed86663f41a8368e6393a8631ff
yszhang95 <zysright@mail.ustc.edu.cn>
2019-03-02 11:17:47 -0600

    add new content to draw eff of daughter vs p

--- promptd0/mtdEff.C
+++ promptd0/mtdEff.C
@@ -18,29 +18,57 @@ inline float invBetaKaon(const float& p){
 
 void mtdEff()
 {
-   TFile* fFunc = new TFile("fFuncDInvBeta.root");
+   //TFile* fFunc = new TFile("fFuncDInvBeta.root");
    TF1 *fExpPion, *fExpKaon, *fPoly2Pion, *fPoly2Kaon;
-   fFunc->GetObject("fExpPion_dInvBetaRMS", fExpPion);
-   fFunc->GetObject("fExpKaon_dInvBetaRMS", fExpKaon);
-   fFunc->GetObject("fPoly2Pion_dInvBetaRMS", fPoly2Pion);
-   fFunc->GetObject("fPoly2Kaon_dInvBetaRMS", fPoly2Kaon);
+   //fFunc->GetObject("fExpPion_dInvBetaRMS", fExpPion);
+   //fFunc->GetObject("fExpKaon_dInvBetaRMS", fExpKaon);
+   //fFunc->GetObject("fPoly2Pion_dInvBetaRMS", fPoly2Pion);
+   //fFunc->GetObject("fPoly2Kaon_dInvBetaRMS", fPoly2Kaon);
+
+   fExpPion = new TF1("fExpPion_dInvBetaRMS","0.005 + 0.017*exp(-x/2.8)", 0.8, 10);
+   fExpKaon = new TF1("fExpKaon_dInvBetaRMS","0.005 + 0.017*exp(-x/2.8)", 0.8, 10);
 
    TFile* f1 = new TFile("matchPromptD0_fullSample.root");
    TNtuple* tp = (TNtuple*) f1->Get("PromptD");
    matchD* t = new matchD(tp);
    std::cout << t->GetEntries() << std::endl;
 
-   bool daughterEff = true;
+   const bool daughterEff_Y_zeroPt = false;
+   const bool daughterEff_P = true;
 
-   TH1F* hPionEff = new TH1F("hPionEff", "hPionEff", 12, -3, 3);
-   TH1F* hPion3sigmaEff = new TH1F("hPion3sigmaEff", "hPion3sigmaEff", ana::nuOfY, -3, 3);
-   TH1F* hPion2sigmaEff = new TH1F("hPion2sigmaEff", "hPion2sigmaEff", ana::nuOfY, -3, 3);
-   TH1F* hPion1sigmaEff = new TH1F("hPion1sigmaEff", "hPion1sigmaEff", ana::nuOfY, -3, 3);
 
-   TH1F* hKaonEff = new TH1F("hKaonEff", "hKaonEff", ana::nuOfY, -3, 3);
-   TH1F* hKaon3sigmaEff = new TH1F("hKaon3sigmaEff", "hKaon3sigmaEff", ana::nuOfY, -3, 3);
-   TH1F* hKaon2sigmaEff = new TH1F("hKaon2sigmaEff", "hKaon2sigmaEff", ana::nuOfY, -3, 3);
-   TH1F* hKaon1sigmaEff = new TH1F("hKaon1sigmaEff", "hKaon1sigmaEff", ana::nuOfY, -3, 3);
+	TH1F* hPionEff;
+	TH1F* hPion3sigmaEff;
+	TH1F* hPion2sigmaEff;
+	TH1F* hPion1sigmaEff;
+	                    ;
+	TH1F* hKaonEff;
+	TH1F* hKaon3sigmaEff;
+	TH1F* hKaon2sigmaEff;
+	TH1F* hKaon1sigmaEff;
+   if(daughterEff_Y_zeroPt){
+      hPionEff = new TH1F("hPionEff", "hPionEff", ana::nuOfY, -3, 3);
+      hPion3sigmaEff = new TH1F("hPion3sigmaEff", "hPion3sigmaEff", ana::nuOfY, -3, 3);
+      hPion2sigmaEff = new TH1F("hPion2sigmaEff", "hPion2sigmaEff", ana::nuOfY, -3, 3);
+      hPion1sigmaEff = new TH1F("hPion1sigmaEff", "hPion1sigmaEff", ana::nuOfY, -3, 3);
+
+      hKaonEff = new TH1F("hKaonEff", "hKaonEff", ana::nuOfY, -3, 3);
+      hKaon3sigmaEff = new TH1F("hKaon3sigmaEff", "hKaon3sigmaEff", ana::nuOfY, -3, 3);
+      hKaon2sigmaEff = new TH1F("hKaon2sigmaEff", "hKaon2sigmaEff", ana::nuOfY, -3, 3);
+      hKaon1sigmaEff = new TH1F("hKaon1sigmaEff", "hKaon1sigmaEff", ana::nuOfY, -3, 3);
+   }
+
+   if(daughterEff_P){
+      hPionEff = new TH1F("hPionEff", "hPionEff", 100, 0, 10);
+      hPion3sigmaEff = new TH1F("hPion3sigmaEff", "hPion3sigmaEff", 100, 0, 10);
+      hPion2sigmaEff = new TH1F("hPion2sigmaEff", "hPion2sigmaEff", 100, 0, 10);
+      hPion1sigmaEff = new TH1F("hPion1sigmaEff", "hPion1sigmaEff", 100, 0, 10);
+
+      hKaonEff = new TH1F("hKaonEff", "hKaonEff", 100, 0, 10);
+      hKaon3sigmaEff = new TH1F("hKaon3sigmaEff", "hKaon3sigmaEff", 100, 0, 10);
+      hKaon2sigmaEff = new TH1F("hKaon2sigmaEff", "hKaon2sigmaEff", 100, 0, 10);
+      hKaon1sigmaEff = new TH1F("hKaon1sigmaEff", "hKaon1sigmaEff", 100, 0, 10);
+   }
 
    for(Long64_t ientry=0; ientry<t->GetEntries(); ientry++){
       t->GetEntry(ientry);
@@ -48,7 +76,6 @@ void mtdEff()
       const int iy = whichY(t->y);
       if( iy == -1 ) continue;
 
-      if(t->pT > 0.5) continue;
 
       // require eta<1.4 ? pT > 0.8 : pT > 0.5
       //if(std::fabs(t->pTD1<0.8) && t->EtaD1<1.4) continue;
@@ -61,7 +88,8 @@ void mtdEff()
       const float pD1 = t->pTD1 * std::cosh(t->EtaD1);
       const float pD2 = t->pTD2 * std::cosh(t->EtaD2);
 
-      if(daughterEff){
+      if(daughterEff_Y_zeroPt){
+         if(t->pT > 0.5) continue;
          if(t->flavor == 1){
             bool is3sigmaPion = false;
             bool is3sigmaKaon = false;
@@ -116,8 +144,63 @@ void mtdEff()
             if(is1sigmaPion && t->isMtdDau2) hPion1sigmaEff->Fill(t->y);
          }
       }
+      if(daughterEff_P){
+         if(t->flavor == 1){
+            bool is3sigmaPion = false;
+            bool is3sigmaKaon = false;
+
+            bool is2sigmaPion = false;
+            bool is2sigmaKaon = false;
+
+            bool is1sigmaPion = false;
+            bool is1sigmaKaon = false;
+
+            if(t->isMtdDau1) hPionEff->Fill(pD1);
+            if(t->isMtdDau1) is3sigmaPion = std::fabs(1./t->beta1_PV - invBetaPion(pD1)) < 3.0 * fExpPion->Eval(pD1);
+            if(t->isMtdDau1) is2sigmaPion = std::fabs(1./t->beta1_PV - invBetaPion(pD1)) < 2.0 * fExpPion->Eval(pD1);
+            if(t->isMtdDau1) is1sigmaPion = std::fabs(1./t->beta1_PV - invBetaPion(pD1)) < 1.0 * fExpPion->Eval(pD1);
+            if(is3sigmaPion && t->isMtdDau1) hPion3sigmaEff->Fill(pD1);
+            if(is2sigmaPion && t->isMtdDau1) hPion2sigmaEff->Fill(pD1);
+            if(is1sigmaPion && t->isMtdDau1) hPion1sigmaEff->Fill(pD1);
+
+            if(t->isMtdDau2) hKaonEff->Fill(pD2);
+            if(t->isMtdDau2) is3sigmaKaon = std::fabs(1./t->beta2_PV - invBetaKaon(pD2)) < 3.0 *fExpKaon->Eval(pD2);
+            if(t->isMtdDau2) is2sigmaKaon = std::fabs(1./t->beta2_PV - invBetaKaon(pD2)) < 2.0 *fExpKaon->Eval(pD2);
+            if(t->isMtdDau2) is1sigmaKaon = std::fabs(1./t->beta2_PV - invBetaKaon(pD2)) < 1.0 *fExpKaon->Eval(pD2);
+            if(is3sigmaKaon && t->isMtdDau2) hKaon3sigmaEff->Fill(pD2);
+            if(is2sigmaKaon && t->isMtdDau2) hKaon2sigmaEff->Fill(pD2);
+            if(is1sigmaKaon && t->isMtdDau2) hKaon1sigmaEff->Fill(pD2);
+         }
+
+         if(t->flavor == -1){
+            bool is3sigmaPion = false;
+            bool is3sigmaKaon = false;
+
+            bool is2sigmaPion = false;
+            bool is2sigmaKaon = false;
+
+            bool is1sigmaPion = false;
+            bool is1sigmaKaon = false;
+
+            if(t->isMtdDau1) hKaonEff->Fill(pD1);
+            if(t->isMtdDau1) is3sigmaKaon = std::fabs(1./t->beta1_PV - invBetaKaon(pD1)) < 3.0 * fExpKaon->Eval(pD1);
+            if(t->isMtdDau1) is2sigmaKaon = std::fabs(1./t->beta1_PV - invBetaKaon(pD1)) < 2.0 * fExpKaon->Eval(pD1);
+            if(t->isMtdDau1) is1sigmaKaon = std::fabs(1./t->beta1_PV - invBetaKaon(pD1)) < 1.0 * fExpKaon->Eval(pD1);
+            if(is3sigmaKaon && t->isMtdDau1) hKaon3sigmaEff->Fill(pD1);
+            if(is2sigmaKaon && t->isMtdDau1) hKaon2sigmaEff->Fill(pD1);
+            if(is1sigmaKaon && t->isMtdDau1) hKaon1sigmaEff->Fill(pD1);
+
+            if(t->isMtdDau2) hPionEff->Fill(pD2);
+            if(t->isMtdDau2) is3sigmaPion = std::fabs(1./t->beta2_PV - invBetaPion(pD2)) < 3.0 * fExpPion->Eval(pD2);
+            if(t->isMtdDau2) is2sigmaPion = std::fabs(1./t->beta2_PV - invBetaPion(pD2)) < 2.0 * fExpPion->Eval(pD2);
+            if(t->isMtdDau2) is1sigmaPion = std::fabs(1./t->beta2_PV - invBetaPion(pD2)) < 1.0 * fExpPion->Eval(pD2);
+            if(is3sigmaPion && t->isMtdDau2) hPion3sigmaEff->Fill(pD2);
+            if(is2sigmaPion && t->isMtdDau2) hPion2sigmaEff->Fill(pD2);
+            if(is1sigmaPion && t->isMtdDau2) hPion1sigmaEff->Fill(pD2);
+         }
+      }
    }
-   if(daughterEff){
+   if(daughterEff_Y_zeroPt){
       TCanvas* c1 = new TCanvas("c1", "PionEff", 500, 550);
       gStyle->SetOptStat(0);
       TH1F* hDrawPion = new TH1F("hDrawPion", "", ana::nuOfY, -3, 3);
@@ -146,6 +229,51 @@ void mtdEff()
       hDrawKaon->GetYaxis()->SetTitle("Kaon Yield Ratio");
       hDrawKaon->GetYaxis()->SetRangeUser(0, 1.3);
       hDrawKaon->GetXaxis()->SetTitle("D0 rapidity");
+      hDrawKaon->Draw();
+      hKaon3sigmaEff->Divide(hKaonEff);
+      hKaon2sigmaEff->Divide(hKaonEff);
+      hKaon1sigmaEff->Divide(hKaonEff);
+      hKaon3sigmaEff->SetLineColor(kGreen-6);
+      hKaon2sigmaEff->SetLineColor(kBlue);
+      hKaon1sigmaEff->SetLineColor(kRed);
+      hKaon3sigmaEff->Draw("same");
+      hKaon2sigmaEff->Draw("same");
+      hKaon1sigmaEff->Draw("same");
+      TLegend *lKaon = new TLegend(0.7, 0.8, 0.9, 0.9);
+      lKaon->AddEntry(hKaon3sigmaEff, "3 RMS", "l");
+      lKaon->AddEntry(hKaon2sigmaEff, "2 RMS", "l");
+      lKaon->AddEntry(hKaon1sigmaEff, "1 RMS", "l");
+      lKaon->Draw();
+   }
+   if(daughterEff_P){
+      TCanvas* c1 = new TCanvas("c1", "PionEff", 500, 550);
+      gStyle->SetOptStat(0);
+      TH1F* hDrawPion = new TH1F("hDrawPion", "", 100, 0, 10);
+      hDrawPion->GetYaxis()->SetTitle("Pion Yield Ratio");
+      hDrawPion->GetYaxis()->SetRangeUser(0, 1.3);
+      hDrawPion->GetXaxis()->SetTitle("p (GeV)");
+      hDrawPion->Draw();
+      hPion3sigmaEff->Divide(hPionEff);
+      hPion2sigmaEff->Divide(hPionEff);
+      hPion1sigmaEff->Divide(hPionEff);
+      hPion3sigmaEff->SetLineColor(kGreen-6);
+      hPion2sigmaEff->SetLineColor(kBlue);
+      hPion1sigmaEff->SetLineColor(kRed);
+      hPion3sigmaEff->Draw("same");
+      hPion2sigmaEff->Draw("same");
+      hPion1sigmaEff->Draw("same");
+      TLegend *lPion = new TLegend(0.7, 0.8, 0.9, 0.9);
+      lPion->AddEntry(hPion3sigmaEff, "3 RMS", "l");
+      lPion->AddEntry(hPion2sigmaEff, "2 RMS", "l");
+      lPion->AddEntry(hPion1sigmaEff, "1 RMS", "l");
+      lPion->Draw();
+
+      TCanvas* c2 = new TCanvas("c2", "KaonEff", 500, 550);
+      gStyle->SetOptStat(0);
+      TH1F* hDrawKaon = new TH1F("hDrawKaon", "", 100, 0, 10);
+      hDrawKaon->GetYaxis()->SetTitle("Kaon Yield Ratio");
+      hDrawKaon->GetYaxis()->SetRangeUser(0, 1.3);
+      hDrawKaon->GetXaxis()->SetTitle("p (GeV)");
       hDrawKaon->Draw();
       hKaon3sigmaEff->Divide(hKaonEff);
       hKaon2sigmaEff->Divide(hKaonEff);
