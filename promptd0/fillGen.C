71b1d8eac15753f0e609184854ecf919f8873df4
Yousen Zhang <zysright@mail.ustc.edu.cn>
2019-03-06 00:06:45 +0100

    calculation

--- promptd0/fillGen.C
+++ promptd0/fillGen.C
@@ -40,23 +40,36 @@ void fillGen()
 
    float pT_gen;
    float y_gen;
+   int status_gen;
    int dau1ID_gen;
    int dau2ID_gen;
 
    t->SetBranchAddress("pT_gen", &pT_gen);
    t->SetBranchAddress("y_gen", &y_gen);
+   t->SetBranchAddress("status_gen", &status_gen);
    t->SetBranchAddress("DauID1_gen", &dau1ID_gen);
    t->SetBranchAddress("DauID2_gen", &dau2ID_gen);
 
+   int n=0;
+   int nStatus2=0;
+   int nStatus2_kpi=0;
    for(Long64_t ientry=0; ientry<t->GetEntries(); ++ientry){
       t->GetEntry(ientry);
 
       const int iy = whichY(y_gen);
       if( iy == -1 ) continue;
+      if(status_gen == 2) nStatus2++;
+      if(status_gen==2 && ((std::fabs(dau1ID_gen) == 211 && std::fabs(dau2ID_gen) == 321) || (std::fabs(dau1ID_gen) == 321 && std::fabs(dau2ID_gen) == 211)))
+         nStatus2_kpi++;
 
       if((std::fabs(dau1ID_gen) == 211 && std::fabs(dau2ID_gen) == 321) || (std::fabs(dau1ID_gen) == 321 && std::fabs(dau2ID_gen) == 211))
             hPt[iy]->Fill(pT_gen);
+      if((std::fabs(dau1ID_gen) == 211 && std::fabs(dau2ID_gen) == 321) || (std::fabs(dau1ID_gen) == 321 && std::fabs(dau2ID_gen) == 211))
+         n++;
    }
+   std::cout << "pass" << n << std::endl;
+   std::cout << "status2" << nStatus2 << std::endl;
+   std::cout << "status2 pass" << nStatus2_kpi << std::endl;
    TFile* fout;
    fout = new TFile("genPt.root", "recreate");
    for(int iy=0; iy<ana::nuOfY; iy++)
