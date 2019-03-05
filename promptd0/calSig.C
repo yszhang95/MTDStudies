285fba10d334b50e66dfe1ee769adf73439bcf00
yszhang95 <zysright@mail.ustc.edu.cn>
2019-03-05 08:50:55 -0600

    fix bugs

--- promptd0/calSig.C
+++ promptd0/calSig.C
@@ -13,16 +13,11 @@ void calSig()
    TH1F* hSignalMassMtd[ana::nuOfY];
    TH1F* hBkgMass[ana::nuOfY];
    TH1F* hBkgMassMtd[ana::nuOfY];
-   TH1F* hGenAllPt;
+   TH1F* hGenPtMidY;
    TH1F* hData;
 
    for(int iy=0; iy<ana::nuOfY; iy++){
       f1->GetObject(Form("hPt%d", iy), hGenPt[iy]);
-      if(iy==0) {
-         hGenAllPt = (TH1F*) hGenPt[0]->Clone();
-      } else{
-         hGenAllPt->Add(hGenPt[iy]);
-      }
       f3->GetObject(Form("hPt%d", iy), hRecoPt[iy]);
       f3->GetObject(Form("hPtMtd%d", iy), hRecoPtMtd[iy]);
       f3->GetObject(Form("hMass%d", iy), hSignalMass[iy]);
@@ -32,14 +27,19 @@ void calSig()
    }
    f2->GetObject("Table 3/Hist1D_y1", hData);
 
+   hGenPtMidY = (TH1F*) hGenPt[4]->Clone();
+   hGenPtMidY->Add(hGenPt[5]);
+   hGenPtMidY->Add(hGenPt[6]);
+   hGenPtMidY->Add(hGenPt[7]);
+
    // try to scale gen spectrum
-   double yield_mc_Pt2_3GeV = hGenAllPt->Integral(20, 30);
-   double yield_data_Pt2_3GeV = hData->GetBinContent(1) * ana::TAA0_100 * ana::pbOvermb * 1 *ana::GeV * ana::evts_sim_MB * ana::BR * 2; // 2 is because of the data is for (D+Dbar)/2
+   double yield_mc_Pt2_3GeV = hGenPtMidY->Integral(20, 30);
+   double yield_data_Pt2_3GeV = hData->GetBinContent(1) * ana::TAA0_100 * ana::pbOvermb * 1 *ana::GeV * ana::evts_data_MB * ana::BR * 2; // 2 is because of the data is for (D+Dbar)/2
    double scale_factor = yield_data_Pt2_3GeV / yield_mc_Pt2_3GeV;
    std::cout << scale_factor << std::endl;
 
 
-   TH1F* hObs[ana::nuOfY];
+   TH1F* hObsMtd[ana::nuOfY];
    TH1F* hSigMtd = new TH1F("hSigMtd", "", ana::nuOfY, ana::ybin);
    TH1F* hSig = new TH1F("hSig", "", ana::nuOfY, ana::ybin);
 
@@ -53,8 +53,8 @@ void calSig()
       hGenPt[iy]->Scale(scale_factor * ana::evts_sim_MB / ana::evts_data_MB);
       hSignalMassMtd[iy]->Scale(scale_factor * ana::evts_sim_MB / ana::evts_data_MB);
       hBkgMassMtd[iy]->Scale(ana::evts_sim_MB / ana::evts_bkg_MB);
-      hObs[iy] = (TH1F*) hSignalMassMtd[iy]->Clone();
-      hObs[iy]->Add(hBkgMassMtd[iy]);
+      hObsMtd[iy] = (TH1F*) hSignalMassMtd[iy]->Clone();
+      hObsMtd[iy]->Add(hBkgMassMtd[iy]);
 
       int masslw = hSignalMassMtd[iy]->FindBin(ana::mass_lw[iy]);
       int massup = hSignalMassMtd[iy]->FindBin(ana::mass_up[iy]);
@@ -78,19 +78,26 @@ void calSig()
 
       hBMtd->SetBinContent(iy+1, bMtd);
       hB->SetBinContent(iy+1, b);
-
-      std::cout << "eff" << std::endl;
    }
+
+   TLatex* ltx = new TLatex();
    TCanvas* c[ana::nuOfY];
    for(int i=0; i<ana::nuOfY; i++){
-      c[i] = new TCanvas(Form("c%d", i), "", 450, 500);
+      c[i] = new TCanvas(Form("c_%d", i), "", 450, 500);
       gStyle->SetOptStat(0);
-      //hObs[i]->Draw();
+      float max = hObsMtd[i]->GetMaximum();
+      hObsMtd[i]->GetYaxis()->SetRangeUser(max*0.5, max*1.3);
+      hObsMtd[i]->Draw();
+      ltx->DrawLatexNDC(0.5, 0.6, Form("%.1f < y < %.1f"), ana::ybin[i], ana::ybin[i+1]);
    }
 
 
-   TCanvas* c1 = new TCanvas();
+   TCanvas* c1 = new TCanvas("c1", "", 450, 500);
    gStyle->SetOptStat(0);
+   hSigMtd->GetYaxis()->SetTitle("Significance");
+   hSigMtd->GetXaxis()->SetTitle("y");
+   float max = hSigMtd->GetMaximum();
+   hSigMtd->GetYaxis()->SetRangeUser(0, max*1.3);
    hSigMtd->SetLineColor(kRed);
    hSigMtd->Draw();
    hSig->Draw("same");
@@ -99,9 +106,13 @@ void calSig()
    lgd->AddEntry(hSig, "w/o mtd", "lp");
    lgd->Draw();
 
-   TCanvas* c2 = new TCanvas();
+   TCanvas* c2 = new TCanvas("c2", "", 450, 500);
    gStyle->SetOptStat(0);
    hSMtd->SetLineColor(kRed);
+   hS->GetYaxis()->SetTitle("S");
+   hS->GetXaxis()->SetTitle("y");
+   float maxS = hS->GetMaximum();
+   hS->GetYaxis()->SetRangeUser(0, maxS*1.3);
    hS->Draw();
    hSMtd->Draw("same");
    TLegend* lgds = new TLegend(0.7, 0.8, 0.95, 0.95);
@@ -109,9 +120,13 @@ void calSig()
    lgds->AddEntry(hS, "w/o mtd", "lp");
    lgds->Draw();
 
-   TCanvas* c3 = new TCanvas();
+   TCanvas* c3 = new TCanvas("c3", "", 450, 500);
    gStyle->SetOptStat(0);
+   hB->GetYaxis()->SetTitle("B");
+   hB->GetXaxis()->SetTitle("y");
    hBMtd->SetLineColor(kRed);
+   float maxB = hB->GetMaximum();
+   hB->GetYaxis()->SetRangeUser(0, maxB*1.3);
    hB->Draw();
    hBMtd->Draw("same");
    TLegend* lgdb = new TLegend(0.7, 0.8, 0.95, 0.95);
