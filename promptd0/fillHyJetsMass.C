e2812e88ffff32f00cf38cf7862d83cf3e1bda87
Yousen Zhang <zysright@mail.ustc.edu.cn>
2019-03-09 05:04:46 +0100

    fill hyjets mass

---
+++ promptd0/fillHyJetsMass.C
@@ -0,0 +1,89 @@
+#include "myAna.h"
+#include "HyJets.h"
+#include "TMath.h"
+#include <iostream>
+#include "TH1.h"
+#include "TH2.h"
+#include "TH2F.h"
+#include "TStyle.h"
+#include "TCanvas.h"
+#include "TChain.h"
+#include "THashList.h"
+#include "TLatex.h"
+#include "TFileCollection.h"
+#include "TVector3.h"
+#include "TF1.h"
+#include "TH3F.h"
+
+inline float invBetaPion(const float& p){
+      return std::sqrt(1 + std::pow(ana::massPion/p,2));
+}
+
+inline float invBetaKaon(const float& p){
+      return std::sqrt(1 + std::pow(ana::massKaon/p,2));
+}
+
+void fillHyJetsMass()
+{
+   //TChain* chain = new TChain("d0ana/VertexCompositeNtuple");
+   //chain->Add("/eos/cms/store/group/phys_heavyions/MTD/anstahll/VertexCompositeAnalysis/HydJets_mc_mtd_NTUPLE_20190227/Hydjet_5p02TeV_TuneCP5_MTD/HydJets_mc_mtd_NTUPLE_20190227/190227_165027/0001/hyjets_mc_mtd_1165.root");
+   //TFileCollection* fc = new TFileCollection("dum", "", "hyjets_sample.list");
+   
+   TChain* chain = new TChain("d0ana_mc/VertexCompositeNtuple");
+   TFileCollection* fc = new TFileCollection("dum", "", "newhyjets.list");
+   chain->AddFileInfoList(fc->GetList()); 
+   HyJets* t = new HyJets(chain);
+   std::cout << t->GetEntries() << std::endl;
+
+   TF1* fExpBTL = new TF1("fExpBTL_dInvBetaRMS","0.005 + 0.016*exp(-x/4.4)");
+   TF1* fExpETL = new TF1("fExpETL_dInvBetaRMS","0.003 + 0.006*exp(-x/7.6)");
+
+   float massbin[61];
+   for(int i=0; i<61; i++){
+      massbin[i] = i * 0.005 + 1.7;
+   }
+
+   TH3F* hMassVsPtVsY = new TH3F("hMassVsPtVsY", "hMassVsPtVsY", ana::nuOfY, ana::ybin, ana::nuOfPt, ana::ptbin, 60, massbin);
+   TH3F* hMassVsPtVsYCent = new TH3F("hMassVsPtVsYCent", "hMassVsPtVsYCent", ana::nuOfY, ana::ybin, ana::nuOfPt, ana::ptbin, 60, massbin);
+
+   TH3F* hMassVsPtVsYMtd = new TH3F("hMassVsPtVsYMtd", "hMassVsPtVsYMtd", ana::nuOfY, ana::ybin, ana::nuOfPt, ana::ptbin, 60, massbin);
+   TH3F* hMassVsPtVsYCentMtd = new TH3F("hMassVsPtVsYCentMtd", "hMassVsPtVsYCentMtd", ana::nuOfY, ana::ybin, ana::nuOfPt, ana::ptbin, 60, massbin);
+
+   for(Long64_t ientry=0; ientry<t->GetEntries(); ientry++){
+      t->GetEntry(ientry);
+
+      if(std::fabs(t->EtaD1) > 3) continue;
+      if(std::fabs(t->EtaD2) > 3) continue;
+
+      const float pD1 = t->pTD1 * std::cosh(t->EtaD1);
+      const float pD2 = t->pTD2 * std::cosh(t->EtaD2);
+
+      if(std::fabs(t->EtaD1) < 1.4 ? t->pTD1 <= 0.8 : pD1 <= 0.7) continue;
+      if(std::fabs(t->EtaD2) < 1.4 ? t->pTD2 <= 0.8 : pD2 <= 0.7) continue;
+
+      const bool isCentral = t->centrality < 20;
+
+      const float dInvBetaCut1 = std::fabs(t->EtaD1<1.5) ? fExpBTL->Eval(pD1) : fExpETL->Eval(pD1);
+      const float dInvBetaCut2 = std::fabs(t->EtaD2<1.5) ? fExpBTL->Eval(pD2) : fExpETL->Eval(pD2);
+
+      hMassVsPtVsY->Fill(t->y, t->pT, t->mass);
+      if(isCentral) hMassVsPtVsYCent->Fill(t->y, t->pT, t->mass);
+
+      bool is1sigmaPionDau1 = true;
+      bool is1sigmaKaonDau1 = true;
+      bool is1sigmaPionDau2 = true;
+      bool is1sigmaKaonDau2 = true;
+
+      if(t->beta1_PV!=-99) is1sigmaPionDau1 = std::fabs(1./t->beta1_PV - invBetaPion(pD1)) < 1.0 * dInvBetaCut1;
+      if(t->beta1_PV!=-99) is1sigmaKaonDau1 = std::fabs(1./t->beta1_PV - invBetaKaon(pD1)) < 1.0 * dInvBetaCut1;
+      if(t->beta2_PV!=-99) is1sigmaPionDau2 = std::fabs(1./t->beta2_PV - invBetaPion(pD2)) < 1.0 * dInvBetaCut2;
+      if(t->beta2_PV!=-99) is1sigmaKaonDau2 = std::fabs(1./t->beta2_PV - invBetaKaon(pD2)) < 1.0 * dInvBetaCut2;
+
+      if((t->flavor == 1 && is1sigmaPionDau1 && is1sigmaKaonDau2) || (t->flavor == -1 && is1sigmaKaonDau1 && is1sigmaPionDau2)){
+         hMassVsPtVsYMtd->Fill(t->y, t->pT, t->mass);
+         if(isCentral) hMassVsPtVsYCentMtd->Fill(t->y, t->pT, t->mass);
+      }
+   }
+
+   TFile fout("hyjetsMassHists_reRECO_all.root", "recreate");
+}
