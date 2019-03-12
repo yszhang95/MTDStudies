f5a22ed08d3bd534d0efaa79e22e07d2bb41aff8
Yousen Zhang <zysright@mail.ustc.edu.cn>
2019-03-12 03:16:32 +0100

    fill gen

--- promptd0/fillGen.C
+++ promptd0/fillGen.C
@@ -29,15 +29,12 @@ void fillGen()
 
    TChain *t = new TChain("d0ana_mc/genCandidateNtuple");
    //TFileCollection* fc = new TFileCollection("dum", "", "d0signal.list");
-   TFileCollection* fc = new TFileCollection("dum", "", "newd0signal.list");
+   TFileCollection* fc = new TFileCollection("dum", "", "newD0Signal.list");
    t->AddFileInfoList(fc->GetList()); 
    
    std::cout << "total entries: " << t->GetEntries() << std::endl;
 
-   TH1F* hPt[ana::nuOfY];
-   for(int iy=0; iy<ana::nuOfY; iy++){
-      hPt[iy] = new TH1F(Form("hPt%d", iy), "", 100, 0, 10);
-   }
+   TH1D* hGenPtMidY = new TH1D("hGenPtMidY", "", 100, 0, 10);
 
    float pT_gen;
    float y_gen;
@@ -57,14 +54,14 @@ void fillGen()
    for(Long64_t ientry=0; ientry<t->GetEntries(); ++ientry){
       t->GetEntry(ientry);
 
-      const int iy = whichY(y_gen);
-      if( iy == -1 ) continue;
+      if(std::fabs(y_gen)>1) continue;
+
       if(status_gen == 2) nStatus2++;
       if(status_gen==2 && ((std::fabs(dau1ID_gen) == 211 && std::fabs(dau2ID_gen) == 321) || (std::fabs(dau1ID_gen) == 321 && std::fabs(dau2ID_gen) == 211)))
          nStatus2_kpi++;
 
       if((std::fabs(dau1ID_gen) == 211 && std::fabs(dau2ID_gen) == 321) || (std::fabs(dau1ID_gen) == 321 && std::fabs(dau2ID_gen) == 211))
-            hPt[iy]->Fill(pT_gen);
+         hGenPtMidY->Fill(pT_gen);
       if((std::fabs(dau1ID_gen) == 211 && std::fabs(dau2ID_gen) == 321) || (std::fabs(dau1ID_gen) == 321 && std::fabs(dau2ID_gen) == 211))
          n++;
    }
@@ -73,6 +70,5 @@ void fillGen()
    std::cout << "status2 pass" << nStatus2_kpi << std::endl;
    TFile* fout;
    fout = new TFile("genPt_reRECO.root", "recreate");
-   for(int iy=0; iy<ana::nuOfY; iy++)
-      hPt[iy]->Write();
+   hGenPtMidY->Write();
 }
